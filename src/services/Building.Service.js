e067ade797edb8aafa9f26d1db04bfa87be88606
Antukhan <antu.khan.988@gmail.com>
2022-12-22 00:40:08 +0600

    Building service created

---
+++ src/services/Building.Service.js
@@ -0,0 +1,60 @@
+import FetchService from "./Fetch.Service";
+
+// const fetchOne = async (id: number) => {
+//     const config = useRuntimeConfig();
+//     const baseUrl = config.public.apiBase;
+
+//     const modelTest = await new FetchService().getData(
+//         baseUrl + "/model-tests/" + id
+//     );
+
+//     return modelTest;
+// };
+
+const fetchAll = async () => {
+    const config = useRuntimeConfig();
+    const apiUrl = config.public.apiUrl;
+
+    const response = await new FetchService().getData(
+        apiUrl + "/building/"
+    );
+
+
+    return response;
+};
+
+
+const Create = async (data) => {
+    const config = useRuntimeConfig();
+    const apiUrl = config.public.apiUrl;
+
+    const response = await new FetchService().postData(
+        apiUrl + "/building/", data
+    );
+
+    return response;
+};
+
+const Delete = async (id) => {
+    const config = useRuntimeConfig();
+    const apiUrl = config.public.apiUrl;
+
+    const response = await new FetchService().deleteData(
+        apiUrl + "/building/" + id
+    );
+
+    return response;
+};
+
+// const fetchByPageNumber = async (page: number) => {
+//     const config = useRuntimeConfig();
+//     const baseUrl = config.public.apiBase;
+
+//     const modelTest = await new FetchService().getData(
+//         baseUrl + "/model-tests?page=" + page
+//     );
+
+//     return modelTest;
+// };
+
+export default { fetchAll, Create, Delete };
