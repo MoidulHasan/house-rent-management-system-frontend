850c189c8f64c1e841184577423d352937472a58
Antukhan <antu.khan.988@gmail.com>
2022-12-24 21:35:07 +0600

    Building details page created

--- .nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///home/antukhan/Work/personal-project/house-rent-management-system/frontend/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
---
+++ src/pages/building/[Building_Name].vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+		{{ Building_Name }}
+	</div>
+</template>
+
+<script setup lang="ts">
+	const route = useRoute();
+
+	const Building_Name = route.params.Building_Name;
+</script>
+
+<style scoped></style>
