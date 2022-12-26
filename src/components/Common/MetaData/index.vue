3f25e0dbad275c1e36a0d7be334bd5887718707d
Antukhan <antu.khan.988@gmail.com>
2022-12-27 03:06:04 +0600

    Metadata component created

---
+++ src/components/Common/MetaData/index.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="meta-tag">
+		<span class="meta-title">{{ dataKey }} : </span>
+		<span class="meta-data ml-3"> {{ value }}</span>
+	</div>
+</template>
+
+<script setup lang="ts">
+	// props
+	const props = defineProps<{
+		dataKey: string;
+		value: string;
+	}>();
+</script>
+
+<style lang="scss" scoped>
+	.meta-tag {
+		padding-top: 0px;
+		padding-bottom: 0px;
+	}
+	.meta-title {
+		font-family: "Poppins";
+		font-style: normal;
+		font-weight: 500;
+		font-size: 15px;
+
+		color: #616161;
+	}
+
+	.meta-data {
+		font-family: "Poppins";
+		font-style: normal;
+		font-weight: 500;
+		font-size: 15px;
+
+		color: #596c91;
+	}
+</style>
