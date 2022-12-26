be82d4531dc513f41adbecd32a5d25b3fcc3c787
Antukhan <antu.khan.988@gmail.com>
2022-12-27 02:18:40 +0600

    Renter delete and update mathod created

--- src/services/Renter.Service.js
+++ src/services/Renter.Service.js
@@ -25,4 +25,27 @@ const Create = async (data) => {
 };
 
 
-export default { fetchAll, Create };
+const Delete = async (id) => {
+    const config = useRuntimeConfig();
+    const apiUrl = config.public.apiUrl;
+
+    const response = await new FetchService().deleteData(
+        apiUrl + "/renters/" + id
+    );
+
+    return response;
+};
+
+
+const Update = async (id, data) => {
+    const config = useRuntimeConfig();
+    const apiUrl = config.public.apiUrl;
+
+    const response = await new FetchService().putData(
+        apiUrl + "/renters/" + id, data
+    );
+
+    return response;
+};
+
+export default { fetchAll, Create, Delete, Update };
