bb51c73c340e30e097e6566fe65bb6367e63fa0a
Antukhan <antu.khan.988@gmail.com>
2023-01-08 15:13:31 +0600

    Admin panel functionality seperated to admin folder

---
+++ src/pages/admin/index.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>
+		<PageBreadcrumb :bread-crumb-item="breadCrumbItems" :start="true" />
+	</div>
+</template>
+
+<script setup>
+	import Appartment from "~/services/Appartment.Service";
+	import Renter from "~/services/Renter.Service";
+
+	import { useStore } from "~~/src/stores/store";
+
+	// define page meta
+	definePageMeta({
+		title: "Dashboard",
+		middleware: ["auth"],
+	});
+
+	// hooks
+	const store = useStore();
+
+	// states
+	const breadCrumbItems = {
+		label: "Home",
+		url: "/",
+		item: {
+			label: "Dashboard",
+			url: "/Dashboard",
+		},
+	};
+</script>
+
+<style lang="scss" scoped></style>
